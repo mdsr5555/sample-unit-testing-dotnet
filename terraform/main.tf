8b17f0b5f172721382bcf72f4b43238de5e2e33b
Mudassar Sarfraz <sahil_k_mudassar@yahoo.com>
2026-04-15 17:01:29 +0100

    asp change to F1

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_service_plan" "this" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
   tags                = var.tags
 }
 
