088611fdffde6c5f87a76f7cbf2559dc2a74b4d6
Mudassar Sarfraz <sahil_k_mudassar@yahoo.com>
2026-04-20 12:19:36 +0100

    fix renaming

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_log_analytics_workspace" "this" {
   name                = var.log_analytics_workspace_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
   tags                = var.tags
@@ -25,8 +25,8 @@ resource "azurerm_log_analytics_workspace" "this" {
 
 resource "azurerm_application_insights" "this" {
   name                = var.application_insights_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   workspace_id        = azurerm_log_analytics_workspace.this.id
   application_type    = "web"
   tags                = var.tags
@@ -34,8 +34,8 @@ resource "azurerm_application_insights" "this" {
 
 resource "azurerm_service_plan" "this" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
   tags                = var.tags
@@ -43,8 +43,8 @@ resource "azurerm_service_plan" "this" {
 
 resource "azurerm_linux_web_app" "webapp" {
   name                = var.web_app_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.this.id
   https_only          = true
   tags                = var.tags
